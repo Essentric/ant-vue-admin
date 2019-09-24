eae767c848c3e2e2b59c2c42d644ee5103b8bc95
Essentric <790012863@qq.com>
2019-09-24 20:06:39 +0800

    feat: 按需引入antd-vue 组件

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,8 @@ module.exports = {
   presets: [
     '@vue/app',
   ],
+  // 安装 babel-plugin-import 插件后如下配置可按需引入 ant-design-vue 组件
+  plugins: [
+    ['import', { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: 'css' }],
+  ],
 };
