1da38d952bab83b424dfca53426ceaf70ce95567
Essentric <790012863@qq.com>
2019-10-10 23:08:41 +0800

    feat: login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,15 +3,15 @@
     <div class="form-wrap">
       <ul class="form-content">
         <li class="form-item input-fill">
-          <input class="input" type="text" placeholder="Username">
+          <input v-model="loginForm.username" class="input" type="text" placeholder="Username">
           <label class="label">Username</label>
         </li>
         <li class="form-item input-fill">
-          <input class="input" type="password" placeholder="password">
+          <input v-model="loginForm.password" class="input" type="password" placeholder="password">
           <label class="label">Password</label>
         </li>
         <li class="form-item">
-          <button class="login-button">Login</button>
+          <button class="login-button" @click="handleLogin">Login</button>
         </li>
       </ul>
     </div>
@@ -21,6 +21,17 @@
 <script>
 export default {
   name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    handleLogin() {},
+  },
 };
 </script>
 <style lang="scss" scoped>
