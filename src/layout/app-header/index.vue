4c7d540e95a3afd22add093838f5cbf54de22d89
Essentric <790012863@qq.com>
2019-10-25 20:49:21 +0800

    feat: 实现全屏切换功能

--- src/layout/app-header/index.vue
+++ src/layout/app-header/index.vue
@@ -6,9 +6,9 @@
       面包屑
     </div>
     <ul class="header-menu">
-      <li class="menu-item">
-        <a-icon class="fullscreen-icon" type="fullscreen" />
-        <!-- <a-icon type="fullscreen-exit" /> -->
+      <li class="menu-item" @click="toggleFullscreen">
+        <a-icon v-if="isFullscreen" type="fullscreen-exit" />
+        <a-icon v-else type="fullscreen" />
       </li>
       <li class="menu-item">
         <a-badge count="15" :offset="[0, -2]">
@@ -38,6 +38,33 @@ export default {
   },
   computed: {
     ...mapState(['headerFixed', 'collapsed']),
+  },
+  data() {
+    return {
+      isFullscreen: false,
+    };
+  },
+  methods: {
+    // 切换全屏
+    toggleFullscreen() {
+      const el = document.documentElement;
+      const fullScreen = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
+      // eslint-disable-next-line
+      const exitScreen = document.exitFullscreen || document.mozCancelFullScreen || document.webkitCancelFullScreen || document.msExitFullscreen;
+      if (!this.isFullscreen) {
+        if (fullScreen) {
+          fullScreen.call(el);
+          this.isFullscreen = true;
+        } else if (window.ActiveXObject) {
+          // 万恶的IE
+          const wscript = new window.ActiveXObject('WScript.Shell');
+          wscript.SendKeys('{F11}');
+        }
+      } else {
+        exitScreen.call(document);
+        this.isFullscreen = false;
+      }
+    },
   },
 };
 </script>
